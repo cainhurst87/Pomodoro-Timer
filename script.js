8ddfa338401b48349720b9a905277eaaeea711ff
Luiz Leal <143563882+cainhurst87@users.noreply.github.com>
2025-03-13 19:33:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    // Constants
     let pomodoroDuration = 25 * 60;
     let shortBreakDuration = 5 * 60;
     let longBreakDuration = 15 * 60;
@@ -8,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let isPaused = false;
     let timerInterval;
 
+    // Get the elements from the DOM 
     const timeDisplay = document.getElementById("time-display");
     const pomodoroBtn = document.getElementById("pomodoro-btn");
     const shortBreakBtn = document.getElementById("short-break-btn");
@@ -19,12 +21,20 @@ document.addEventListener("DOMContentLoaded", function() {
     const saveCustomizationBtn = document.getElementById("save-customization-btn");
     const customizeModal = document.getElementById("customizeModal");
 
+    // Start the timers configurable by the user as the default values
+    let userPomodoroDuration = pomodoroDuration;
+    let userShortBreakDuration = shortBreakDuration;
+    let userLongBreakDuration = longBreakDuration;
+    let currentMode = 'pomodoro'; // Track the current mode
+
+    // Initialize the timer display and update every second
     function updateTimeDisplay(time) {
         const minutes = Math.floor(time / 60);
         const seconds = time % 60;
         timeDisplay.textContent = `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
     }
 
+    // Start the timer, decrease it every second and alert when it ends 
     function startTimer() {
         if (isRunning) return;
         isRunning = true;
@@ -36,6 +46,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 clearInterval(timerInterval);
                 isRunning = false;
                 alert("Time's up!");
+                resetTimer();
             }
         }, 1000);
 
@@ -47,6 +58,7 @@ document.addEventListener("DOMContentLoaded", function() {
         longBreakBtn.disabled = true;
     }
 
+    // Pause the timer
     function pauseTimer() {
         if (!isRunning) return;
         clearInterval(timerInterval);
@@ -57,11 +69,18 @@ document.addEventListener("DOMContentLoaded", function() {
         pauseBtn.classList.add("active");
     }
 
+    // Stop the timer and reset it to the value given by the user
     function stopTimer() {
         clearInterval(timerInterval);
         isRunning = false;
         isPaused = false;
-        currentTime = pomodoroDuration;
+        if (currentMode === 'pomodoro') {
+            currentTime = userPomodoroDuration;
+        } else if (currentMode === 'shortBreak') {
+            currentTime = userShortBreakDuration;
+        } else if (currentMode === 'longBreak') {
+            currentTime = userLongBreakDuration;
+        }
         updateTimeDisplay(currentTime);
 
         pomodoroBtn.disabled = false;
@@ -72,8 +91,29 @@ document.addEventListener("DOMContentLoaded", function() {
         stopBtn.classList.remove("active");
     }
 
+    // Reset the timer to the value given by the user
+    function resetTimer(){
+        if (currentMode === 'pomodoro') {
+            currentTime = userPomodoroDuration;
+        } else if (currentMode === 'shortBreak') {
+            currentTime = userShortBreakDuration;
+        } else if (currentMode === 'longBreak') {
+            currentTime = userLongBreakDuration;
+        }
+        updateTimeDisplay(currentTime);
+
+        pomodoroBtn.disabled = false;
+        shortBreakBtn.disabled = false;
+        longBreakBtn.disabled = false;
+        playBtn.classList.remove("active");
+        pauseBtn.classList.remove("active");
+        stopBtn.classList.remove("active");
+    }
+
+    // Setup timer to pomodoro mode
     function handlePomodoroClick() {
-        currentTime = pomodoroDuration;
+        currentMode = 'pomodoro';
+        currentTime = userPomodoroDuration;
         updateTimeDisplay(currentTime);
         if (isRunning) return;
         pomodoroBtn.classList.add("active");
@@ -81,8 +121,10 @@ document.addEventListener("DOMContentLoaded", function() {
         longBreakBtn.classList.remove("active");
     }
 
+    // Setup timer to short break mode
     function handleShortBreakClick() {
-        currentTime = shortBreakDuration;
+        currentMode = 'shortBreak';
+        currentTime = userShortBreakDuration;
         updateTimeDisplay(currentTime);
         if (isRunning) return;
         shortBreakBtn.classList.add("active");
@@ -90,8 +132,10 @@ document.addEventListener("DOMContentLoaded", function() {
         longBreakBtn.classList.remove("active");
     }
 
+    // Setup timer to long break mode
     function handleLongBreakClick() {
-        currentTime = longBreakDuration;
+        currentMode = 'longBreak';
+        currentTime = userLongBreakDuration;
         updateTimeDisplay(currentTime);
         if (isRunning) return;
         longBreakBtn.classList.add("active");
@@ -99,6 +143,7 @@ document.addEventListener("DOMContentLoaded", function() {
         shortBreakBtn.classList.remove("active");
     }
 
+    // Handle play button click
     function handlePlayClick() {
         if (isRunning) return;
         if (isPaused) {
@@ -109,34 +154,47 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
+    // Handle pause button click
     function handlePauseClick() {
         pauseTimer();
     }
 
+    // Handle stop button click
     function handleStopClick() {
         stopTimer();
     }
 
+    // Handle restart button click
     function handleRestartClick() {
         stopTimer();
     }
 
+    // Handle modal menus's save  button click
     function handleSaveCustomizationClick() {
-        const pomodoroMinutes = parseInt(document.getElementById("pomodoro-minutes").value) || 25;
-        const shortBreakMinutes = parseInt(document.getElementById("short-break-minutes").value) || 5;
-        const longBreakMinutes = parseInt(document.getElementById("long-break-minutes").value) || 15;
 
-        pomodoroDuration = pomodoroMinutes * 60;
-        shortBreakDuration = shortBreakMinutes * 60;
-        longBreakDuration = longBreakMinutes * 60;
+        // Get the values from the input fields and validate them to be in the range of 1 to 60
+        const pomodoroMinutes = Math.min(Math.max(parseInt(document.getElementById("pomodoro-minutes").value), 1), 60) || 25;
+        const shortBreakMinutes = Math.min(Math.max(parseInt(document.getElementById("short-break-minutes").value), 1), 15) || 5;
+        const longBreakMinutes = Math.min(Math.max(parseInt(document.getElementById("long-break-minutes").value), 1), 30) || 15;
 
-        currentTime = pomodoroDuration;
+        userPomodoroDuration = pomodoroMinutes * 60;
+        userShortBreakDuration = shortBreakMinutes * 60;
+        userLongBreakDuration = longBreakMinutes * 60;
+
+        if (currentMode === 'pomodoro') {
+            currentTime = userPomodoroDuration;
+        } else if (currentMode === 'shortBreak') {
+            currentTime = userShortBreakDuration;
+        } else if (currentMode === 'longBreak') {
+            currentTime = userLongBreakDuration;
+        }
         updateTimeDisplay(currentTime);
 
         const modal = bootstrap.Modal.getInstance(document.getElementById('customizeModal'));
         modal.hide();
     }
 
+    // Event listeners
     customizeModal.addEventListener("show.bs.modal", function() {
         stopTimer();
         pomodoroBtn.classList.add("active");
@@ -144,6 +202,7 @@ document.addEventListener("DOMContentLoaded", function() {
         longBreakBtn.classList.remove("active");
     });
 
+    // Event listeners
     pomodoroBtn.addEventListener("click", handlePomodoroClick);
     shortBreakBtn.addEventListener("click", handleShortBreakClick);
     longBreakBtn.addEventListener("click", handleLongBreakClick);
